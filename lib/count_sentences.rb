ab80452f9ab1095f50204db8c10b30fdad6b7f7c
Srija S <srijakumar7@gmail.com>
2018-05-06 17:23:01 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+  count = 0
   def count_sentences
-
+    array = self.split
+    array.each do |key|
+      if key = "."
+        count = count + 1
+      end
+    end
   end
+  
 end
